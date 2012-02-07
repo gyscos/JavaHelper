ee6f1335f569ce4a0722487a5e40876266f12475
Alexandre Bury <alexandre.bury@gmail.com>
2012-02-07 18:55:17 +0100

    Updated Repeat Action to use Value instead of int

--- src/com/helper/plan/action/Repeat.java
+++ src/com/helper/plan/action/Repeat.java
@@ -3,19 +3,21 @@ package com.helper.plan.action;
 import java.util.List;
 
 import com.helper.plan.Memory;
+import com.helper.plan.value.Value;
 
 public class Repeat implements Action {
 
-    int          n;
+    Value        v;
     List<Action> A;
 
-    public Repeat(int n, List<Action> A) {
+    public Repeat(Value v, List<Action> A) {
         this.A = A;
-        this.n = n;
+        this.v = v;
     }
 
     @Override
     public void act(Memory memory) {
+        int n = v.getInt(memory);
         for (int i = 0; i < n; i++)
             for (Action a : A)
                 a.act(memory);
