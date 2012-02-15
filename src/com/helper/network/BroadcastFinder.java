a28a3debf7da10a9a2babbd3974ca174f7e6e803
Alexandre Bury <alexandre.bury@gmail.com>
2012-02-15 09:33:39 +0100

    Added UDP broadcast framework
    Defines BroadcastProvider and BroadcastFinder, that are designed to work
    together on the same port (on different machines).

---
+++ src/com/helper/network/BroadcastFinder.java
@@ -0,0 +1,71 @@
+package com.helper.network;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public abstract class BroadcastFinder {
+    DatagramSocket socket;
+    boolean        running = false;
+    Thread         thread;
+    int            port;
+    String         broadcastIp;
+
+    byte[]         buffer  = new byte[64];
+
+    public BroadcastFinder(String broadcastIp, int port) {
+        this.broadcastIp = broadcastIp;
+        this.port = port;
+    }
+
+    public void ask(InetAddress addr) throws IOException {
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, addr, port);
+        socket.send(packet);
+    }
+
+    public void end() {
+        socket.close();
+    }
+
+    public abstract void onFind(InetAddress addr, String name);
+
+    public void run() throws IOException {
+        while (running) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            String name = new String(packet.getData());
+            InetAddress addr = packet.getAddress();
+            onFind(addr, name);
+        }
+    }
+
+    public void setup() throws IOException {
+        socket = new DatagramSocket(port);
+
+        InetAddress addr = InetAddress.getByName(broadcastIp);
+        ask(addr);
+    }
+
+    public BroadcastFinder start() {
+        running = true;
+        thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    setup();
+                    run();
+                    end();
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        return this;
+    }
+}
---
+++ src/com/helper/network/BroadcastProvider.java
@@ -0,0 +1,74 @@
+package com.helper.network;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class BroadcastProvider {
+    DatagramSocket socket;
+    boolean        running = false;
+    Thread         thread;
+    String         name;
+    int            port;
+    String         broadcastIp;
+
+    byte[]         buffer  = new byte[64];
+
+    public BroadcastProvider(String name, String broadcastIp, int port) {
+        this.name = name;
+        this.port = port;
+        this.broadcastIp = broadcastIp;
+    }
+
+    public void end() {
+        socket.close();
+    }
+
+    public void run() throws IOException {
+        while (running) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            InetAddress addr = packet.getAddress();
+
+            // Send him our name.
+            tell(addr);
+        }
+    }
+
+    public void setup() throws IOException {
+        socket = new DatagramSocket(port);
+
+        InetAddress addr = InetAddress.getByName(broadcastIp);
+        tell(addr);
+    }
+
+    public BroadcastProvider start() {
+        running = true;
+        thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    setup();
+                    run();
+                    end();
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        return this;
+    }
+
+    private void tell(InetAddress addr) throws IOException {
+        // Send a broadcast
+        byte[] byteName = name.getBytes();
+        DatagramPacket packet = new DatagramPacket(byteName, byteName.length, addr, port);
+        socket.send(packet);
+    }
+}
