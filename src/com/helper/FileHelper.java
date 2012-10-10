e2fd833b0551ea2e2c8b70c71b3305250e18852b
Alexandre Bury <alexandre.bury@gmail.com>
2012-10-10 10:30:03 +0200

    Added compression to FileHelper

--- src/com/helper/FileHelper.java
+++ src/com/helper/FileHelper.java
@@ -1,31 +1,52 @@
 package com.helper;
 
-import java.io.UnsupportedEncodingException;
-import java.io.FileNotFoundException;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class FileHelper {
+
     public static void writeToFile(String filename, String content) {
+        writeToFile(filename, content, false);
+    }
+
+    public static void writeToFile(String filename, String content, boolean compress) {
         try {
-            PrintWriter writer = new PrintWriter(filename, "UTF-8");
-            writer.println(content);
-            writer.close();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            writeToFile(new FileOutputStream(filename), content, compress);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static String getContent(String filename) throws FileNotFoundException {
+    static void writeToFile(OutputStream out, String content, boolean compress) {
         try {
-            FileInputStream fis = new FileInputStream(filename);
-            InputStreamReader in = new InputStreamReader(fis, "UTF-8");
-            BufferedReader reader = new BufferedReader(in);
+            OutputStream os = compress ? new GZIPOutputStream(out) : out;
+
+            OutputStreamWriter ow = new OutputStreamWriter(os, "UTF-8");
+            PrintWriter writer = new PrintWriter(ow);
+            writer.println(content);
+            writer.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getContent(String filename) throws FileNotFoundException {
+        return getContent(filename, false);
+    }
+
+    public static String getContent(String filename, boolean decompress) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(filename);
+        return getContent(fis, decompress);
+    }
+
+    static String getContent(InputStream in, boolean decompress) {
+        try {
+            InputStream is = decompress ? new GZIPInputStream(in) : in;
+            InputStreamReader ir = new InputStreamReader(is, "UTF-8");
+            BufferedReader reader = new BufferedReader(ir);
 
             String result = "";
             String line;
