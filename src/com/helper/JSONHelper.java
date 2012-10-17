19cb1b5c4e45a7bb19ced7235a199df21b435a63
Alexandre Bury <alexandre.bury@gmail.com>
2012-10-17 14:05:07 +0200

    Added JSONHelper

---
+++ src/com/helper/JSONHelper.java
@@ -0,0 +1,33 @@
+package com.helper;
+
+import org.json.*;
+import java.util.Iterator;
+
+public class JSONHelper {
+
+    public static void merge(JSONArray source, JSONArray target) throws JSONException {
+        for (int i = 0; i < source.length(); i++) {
+            target.put(source.get(i));
+        }
+    }
+
+    public static void merge(JSONObject source, JSONObject target) throws JSONException {
+        Iterator it = source.keys();
+        while (it.hasNext()) {
+            String key = (String) it.next();
+            // If key is not present, obj = null
+            Object obj = target.opt(key);
+
+            if (obj instanceof JSONObject) {
+                merge(source.getJSONObject(key), target.getJSONObject(key));
+            } else if (obj instanceof JSONArray) {
+                merge(source.getJSONArray(key), target.getJSONArray(key));
+            } else {
+                // Also happens if obj = null
+                target.put(key, source.get(key));
+            }
+        }
+
+    }
+
+}
